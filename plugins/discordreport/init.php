859683745121595572771c25407aa964bf441588
maorninja <maorninja@outlook.com>
2018-01-07 10:36:16 -0800

    another attempt to fix Discord -> Forum

--- plugins/discordreport/init.php
+++ plugins/discordreport/init.php
@@ -2,29 +2,26 @@
 
 $WebHookdata = ["content" => str_replace('@', '[at]', $stuff), "username" => Settings::get("WebHookName"), "avatar_url" => Settings::Get("webhookimage")];
 
-function HelpReport($stuff)
-{
-    $curl = curl_init(Settings::get("helpwebhook"));
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($WebHookdata));
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    $fartz = curl_exec($curl);
+function HelpReport($stuff) {
+	$discordhelpcurl = curl_init(Settings::get("helpwebhook"));
+	curl_setopt($discordhelpcurl, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($discordhelpcurl, CURLOPT_POSTFIELDS, json_encode($WebHookdata));
+	curl_setopt($discordhelpcurl, CURLOPT_RETURNTRANSFER, true);
+	curl_exec($discordhelpcurl);
 }
 
-function DevReport($stuff)
-{
-    $curl = curl_init(Settings::Get("devwebhook"));
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($Webhookdata));
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    $fartz = curl_exec($curl);
+function DevReport($stuff) {
+	$discorddevcurl = curl_init(Settings::get("devwebhook"));
+	curl_setopt($discorddevcurl, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($discorddevcurl, CURLOPT_POSTFIELDS, json_encode($WebHookdata));
+	curl_setopt($discorddevcurl, CURLOPT_RETURNTRANSFER, true);
+	curl_exec($discorddevcurl);
 }
 
-function PostReport($stuff)
-{
-    $curl = curl_init(Settings::Get("ForumWebhook"));
-    curl_setopt($curl, CURLOPT_CUSTOMREQUEST, "POST");
-    curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($Webhookdata));
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    $fartz = curl_exec($curl);
+function PostReport($stuff) {
+	$discordpostcurl = curl_init(Settings::get("ForumWebhook"));
+	curl_setopt($discordpostcurl, CURLOPT_CUSTOMREQUEST, "POST");
+	curl_setopt($discordpostcurl, CURLOPT_POSTFIELDS, json_encode($WebHookdata));
+	curl_setopt($discordpostcurl, CURLOPT_RETURNTRANSFER, true);
+	curl_exec($discordpostcurl);
 }
